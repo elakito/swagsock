15d97859f2e776cda0b9e6c1afbd58de971b3c27
Aki Yoshida <elakito@gmail.com>
2018-06-07 16:57:00 +0200

    move the common demo_*client part into demo_common

---
+++ examples/greeter-client/demo_common.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	apiclient "github.com/elakito/swagsock/examples/greeter-client/client"
+	apimodels "github.com/elakito/swagsock/examples/greeter-client/models"
+	apioperations "github.com/elakito/swagsock/examples/greeter-client/client/operations"
+)
+
+// the common code to run after the client is setup
+// currently, only invokes each operation
+func perform(client *apiclient.GreeterDemo) {
+	// ping
+	pingOK, err := client.Operations.Ping(nil)
+	if err != nil {
+		log.Fatalf("failed to invoke: %v", err)
+	}
+	// print out the json representaiton of the pong object
+	jsonpayload, _ := json.Marshal(pingOK.Payload)
+	fmt.Printf("Ping-Response: %s\n", jsonpayload)
+
+	// echo
+	echoBody := "hi"
+	echoParams := apioperations.NewEchoParams().WithBody(&echoBody)
+	echoOK, err := client.Operations.Echo(echoParams)
+	if err != nil {
+		log.Fatalf("failed to invoke: %v", err)
+	}
+	jsonpayload, _ = json.Marshal(echoOK.Payload)
+	fmt.Printf("Echo-Response: %s\n", jsonpayload)
+
+	// greet
+	greetBody := &apimodels.Body{Name: "foo", Text: "hola"}
+	greetParams := apioperations.NewGreetParams().WithName("bar").WithBody(greetBody)
+	greetOK, err := client.Operations.Greet(greetParams)
+	if err != nil {
+		log.Fatalf("failed to invoke: %v", err)
+	}
+	jsonpayload, _ = json.Marshal(greetOK.Payload)
+	fmt.Printf("Greet-Response: %s\n", jsonpayload)
+
+	// greetStatus
+	getGreetStatusParams := apioperations.NewGetGreetStatusParams().WithName("bar")
+	getGreetStatusOK, err := client.Operations.GetGreetStatus(getGreetStatusParams)
+	if err != nil {
+		log.Fatalf("failed to invoke: %v", err)
+	}
+	jsonpayload, _ = json.Marshal(getGreetStatusOK.Payload)
+	fmt.Printf("GetGreetStatus-Response: %s\n", jsonpayload)
+
+	// greetSummary
+	getGreetSummaryOK, err := client.Operations.GetGreetSummary(nil)
+	if err != nil {
+		log.Fatalf("failed to invoke: %v", err)
+	}
+	jsonpayload, _ = json.Marshal(getGreetSummaryOK.Payload)
+	fmt.Printf("GetGreetSummary-Response: %s\n", jsonpayload)
+}
--- examples/greeter-client/demo_wsclient.go
+++ examples/greeter-client/demo_wsclient.go
@@ -25,8 +25,6 @@ import (
 
 	apiclient "github.com/elakito/swagsock/examples/greeter-client/client"
 	apioperations "github.com/elakito/swagsock/examples/greeter-client/client/operations"
-	apimodels "github.com/elakito/swagsock/examples/greeter-client/models"
-
 	"github.com/elakito/swagsock/swagsock"
 )
 
@@ -50,53 +48,9 @@ func main() {
 	client := newWSClient(transport, strfmt.Default)
 	fmt.Printf("demo_client invoking some operations at target_path %s ...\n", *target_path)
 
-	// ping
-	pingOK, err := client.Operations.Ping(nil)
-	if err != nil {
-		log.Fatalf("failed to invoke: %v", err)
-	}
-
-	// print out the json representaiton of the pong object
-	jsonpayload, _ := json.Marshal(pingOK.Payload)
-	fmt.Printf("Ping-Response: %s\n", jsonpayload)
-
-	// echo
-	echoBody := "hi"
-	echoParams := apioperations.NewEchoParams().WithBody(&echoBody)
-	echoOK, err := client.Operations.Echo(echoParams)
-	if err != nil {
-		log.Fatalf("failed to invoke: %v", err)
-	}
-	jsonpayload, _ = json.Marshal(echoOK.Payload)
-	fmt.Printf("Echo-Response: %s\n", jsonpayload)
-
-	// greet
-	greetBody := &apimodels.Body{Name: "foo", Text: "hola"}
-	greetParams := apioperations.NewGreetParams().WithName("bar").WithBody(greetBody)
-	greetOK, err := client.Operations.Greet(greetParams)
-	if err != nil {
-		log.Fatalf("failed to invoke: %v", err)
-	}
-	jsonpayload, _ = json.Marshal(greetOK.Payload)
-	fmt.Printf("Greet-Response: %s\n", jsonpayload)
-
-	// greetStatus
-	getGreetStatusParams := apioperations.NewGetGreetStatusParams().WithName("bar")
-	getGreetStatusOK, err := client.Operations.GetGreetStatus(getGreetStatusParams)
-	if err != nil {
-		log.Fatalf("failed to invoke: %v", err)
-	}
-	jsonpayload, _ = json.Marshal(getGreetStatusOK.Payload)
-	fmt.Printf("GetGreetStatus-Response: %s\n", jsonpayload)
-
-	// greetSummary
-	getGreetSummaryOK, err := client.Operations.GetGreetSummary(nil)
-	if err != nil {
-		log.Fatalf("failed to invoke: %v", err)
-	}
-	jsonpayload, _ = json.Marshal(getGreetSummaryOK.Payload)
-	fmt.Printf("GetGreetSummary-Response: %s\n", jsonpayload)
+	perform(client)
 }
+
 
 /////////
 // currently, this demo client works with the unmodified go-openapi/runtime et al.
